f22918467c32a66c274a85af438f61ce888eb794
rollonrenren-sketch <rollonrenren@gmail.com>
2025-11-02 01:58:44 +0800

    Update tmdb-proxy.js

--- my-website/functions/api/tmdb-proxy.js
+++ my-website/functions/api/tmdb-proxy.js
@@ -1,1 +1,15 @@
+export async function onRequest(context) {
+  const { request, env } = context;
+  const url = new URL(request.url);
+  const query = url.searchParams.get('query') || "";
+  const apiKey = env.API_KEY;
+  const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(query)}`;
+  try {
+    const apiResponse = await fetch(apiUrl);
+    const data = await apiResponse.json();
+    return new Response(JSON.stringify(data), { headers: { "Content-Type": "application/json" } });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+  }
+}
 
